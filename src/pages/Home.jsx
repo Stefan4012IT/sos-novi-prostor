68fb1aaedc9c9d84557a32d5755579e654cceffb
stefan4012it <stefanra555@gmail.com>
2025-04-08 16:59:53 +0200

    day8

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,7 @@ import num_4 from '../assets/benefits/nums/num_4_black.svg'
 import benefits_1_img from '../assets/benefits/benefits_1.png'
 import benefits_2_img from '../assets/benefits/benefits_2.png'
 import benefits_3_img from '../assets/benefits/benefits_3.png'
+import benefits_4_img from '../assets/benefits/benefits_4.png'
 
 import prizelje_1 from '../assets/prizemlje_1.jpg'
 import prizelje_2 from '../assets/prizemlje_2.jpg'
@@ -45,7 +46,7 @@ const benefitsData = [
     { img: benefits_1_img, num: num_1, title: "Designed to inspire:", subtitle: "najmodernija tehnologija za uspeh u budućnosti", text: "Prostor je dizajniran tako da podstiče inovativne načine razmišljanja i pomeranje granica. Savremene tehnologije i fleksibilni elementi stvaraju dinamično okruženje, dajuci učenicima mogucnost za stvaranje… nesto future." },
     { img: benefits_2_img, num: num_2, title: "Where ideas take shape:", subtitle: "od ideje do akcije", text: "Ideje postaju konkretni projekti, a projekti postaju ciljevi koji se realizuju. Opremljen savremenim tehnologijama i osmišljen za budućnost – prostor koji raste zajedno s generacijama koje dolaze." },
     { img: benefits_3_img, num: num_3, title: "Where trust begins:", subtitle: "uz partnerstvo i saradnju sa roditeljima", text: "Kada roditelji i škola imaju isti cilj, dete dobija najznačajniju podršku na svom putu. A poverenje ne nastaje slučajno - ono je nusprodukt otvorene komunikacije, međusobnog uvažavanja i zajedničke brige o detetu." },
-    { img: benefits_1_img, num: num_4, title: "Ready to grow:", subtitle: "Više mesta za više mogućnosti.", text: "Rastemo zajedno i stvaramo zajednicu koja pomera granice. Prošireni kapaciteti odgovaraju potražnji i omogućavaju većem broju učenika da postanu deo savremenog obrazovanja – drugačijeg, inovativnog i spremnog za budućnost." },
+    { img: benefits_4_img, num: num_4, title: "Ready to grow:", subtitle: "Više mesta za više mogućnosti.", text: "Rastemo zajedno i stvaramo zajednicu koja pomera granice. Prošireni kapaciteti odgovaraju potražnji i omogućavaju većem broju učenika da postanu deo savremenog obrazovanja – drugačijeg, inovativnog i spremnog za budućnost." },
   ];
 
   const zones = [
