51a225c1f2587fdca226401d1feaac8de9d54ce9
stefan4012it <stefanra555@gmail.com>
2025-04-16 14:27:52 +0200

    day15

--- src/components/SectionHero.jsx
+++ src/components/SectionHero.jsx
@@ -58,14 +58,16 @@ const SectionHero = () => {
         <img src={intro_img_1} alt="" className="img-1" />
         <img src={intro_img_2} alt="" className="img-2" />
         <img src={intro_img_3} alt="" className="img-3" />
-        <img src={intro_img_4} alt="" className="img-4" />
-        <img src={intro_img_5} alt="" className="img-5" />
-        <img src={intro_img_6} alt="" className="img-6" />
       </div>
 
       <div className="title">
         <h2>Novi prostorni koncept,</h2>
         <TitleRevealHome />
+      </div>
+      <div className="hero-img">
+        <img src={intro_img_4} alt="" className="img-4" />
+        <img src={intro_img_5} alt="" className="img-5" />
+        <img src={intro_img_6} alt="" className="img-6" />
       </div>
     </section>
   );
