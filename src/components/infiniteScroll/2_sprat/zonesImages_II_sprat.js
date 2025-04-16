5c392985065d162a96c517bbb5ae43db746c50a1
stefan4012it <stefanra555@gmail.com>
2025-04-16 13:50:42 +0200

    day15

--- src/components/infiniteScroll/2_sprat/zonesImages_II_sprat.js
+++ src/components/infiniteScroll/2_sprat/zonesImages_II_sprat.js
@@ -1,11 +1,11 @@
 import sprat_II_1 from '../../../assets/sprat_II/sprat_II_1.jpg'
-import sprat_II_2 from '../../../assets/sprat_II/sprat_II_2.jpg'
+import sprat_II_2 from '../../../assets/sprat_II/sprat_II_2.1.jpg'
 import sprat_II_3 from '../../../assets/sprat_II/sprat_II_3.jpg'
 import sprat_II_4 from '../../../assets/sprat_II/sprat_II_4.jpg'
 import sprat_II_5 from '../../../assets/sprat_II/sprat_II_5.jpg'
 import sprat_II_6 from '../../../assets/sprat_II/sprat_II_6.jpg'
 import sprat_II_7 from '../../../assets/sprat_II/sprat_II_7.jpg'
-import sprat_II_8 from '../../../assets/sprat_II/sprat_II_8.jpg'
+import sprat_II_8 from '../../../assets/sprat_II/sprat_II_8.1.jpg'
 
 export const zoneImages = [
     [sprat_II_1, sprat_II_2], 
--- src/components/infiniteScroll/4_sprat/zonesImages_IV_sprat.js
+++ src/components/infiniteScroll/4_sprat/zonesImages_IV_sprat.js
@@ -1,5 +1,5 @@
 import sprat_IV_1 from '../../../assets/sprat_IV/sprat_IV_1.jpg'
-import sprat_IV_2 from '../../../assets/sprat_IV/sprat_IV_2.jpg'
+import sprat_IV_2 from '../../../assets/sprat_IV/sprat_IV_2.1.jpg'
 import sprat_IV_3 from '../../../assets/sprat_IV/sprat_IV_3.jpg'
 import sprat_IV_4 from '../../../assets/sprat_IV/sprat_IV_4.jpg'
 
