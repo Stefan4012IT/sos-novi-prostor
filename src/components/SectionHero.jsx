a5c5cd0df2ae3b2eb88cce15905370b8628cd471
stefan4012it <stefanra555@gmail.com>
2025-04-16 14:58:01 +0200

    day15

--- src/components/SectionHero.jsx
+++ src/components/SectionHero.jsx
@@ -6,6 +6,8 @@ import intro_img_3 from '../assets/hero/intro_3.jpg'
 import intro_img_4 from '../assets/hero/intro_4.jpg'
 import intro_img_5 from '../assets/hero/intro_1.jpg'
 import intro_img_6 from '../assets/hero/intro_6.jpg'
+import intro_img_7 from '../assets/hero/intro_7.jpg'
+import intro_img_8 from '../assets/hero/intro_8.jpg'
 
 import RotatingWords from "./RotatingWords";
 import TitleRevealHome from "./TitleRevealHome";
@@ -58,16 +60,12 @@ const SectionHero = () => {
         <img src={intro_img_1} alt="" className="img-1" />
         <img src={intro_img_2} alt="" className="img-2" />
         <img src={intro_img_3} alt="" className="img-3" />
+        <img src={intro_img_4} alt="" className="img-4" />
       </div>
 
       <div className="title">
         <h2>Novi prostorni koncept,</h2>
         <TitleRevealHome />
-      </div>
-      <div className="hero-img">
-        <img src={intro_img_4} alt="" className="img-4" />
-        <img src={intro_img_5} alt="" className="img-5" />
-        <img src={intro_img_6} alt="" className="img-6" />
       </div>
     </section>
   );
